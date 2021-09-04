cad97b8307e4fa2c76e2508ada5ce666c08f4a10
Alexander Barakhov <a.barakhov@payvision.com>
2021-09-04 12:57:14 +0200

    initial setup

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="barakhov.github.io" />
+    <img alt="Vue logo" src="../assets/hackerman.jpg" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+img {
+  margin: 0 auto;
+  max-width: 250px;
+}
+</style>
