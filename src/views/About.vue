4b5b7d3042dfc2f56ff1dd90c93aa80427fa31a0
Alexander Barakhov <a.barakhov@payvision.com>
2021-09-11 11:32:50 +0200

    helpers class added

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,6 @@
       <h3>h3 lorem ipsum</h3>
       <h4>h4 lorem ipsum</h4>
       <h5>h5 lorem ipsum</h5>
-      <h6>h6 lorem ipsum</h6>
       <p>
         It's just such a strange, strange concept. Have you ever tasted Elk
         heart? so good for you, one of the leanest meats. I've got some i'll
@@ -27,12 +26,14 @@
         is REALLY illegal? Have you ever tasted Elk heart? so good for you, one
         of the leanest meats. I've got some i'll give you it on the way out.
       </p>
+      <h2>How weird is that? h2</h2>
       <p>
         Every 10 years the cells in your body actually completely change. Every
         10 years the cells in your body actually completely change. Well no
         because... Do you know why weed is REALLY illegal? Jamie pull up that
         gif of Tortoises fucking! Jaimie could you get that article up?
       </p>
+      <h3>Alex Barakhov h3</h3>
       <p>
         We put a monetary on actual life itself. Every 10 years the cells in
         your body actually completely change. Have you ever tasted Elk heart? so
@@ -41,12 +42,22 @@
         pounds? Jesus those things will tear you to shreds. Do you know why weed
         is REALLY illegal?
       </p>
+      <h4>Skills / Education / Work Experience h4</h4>
       <p>
         Every 10 years the cells in your body actually completely change.
         <i>Brings the mic closer</i> Have you ever tasted Elk heart? so good for
         you, one of the leanest meats. I've got some i'll give you it on the way
         out. You should have your own podcast. Every 10 years the cells in your
         body actually completely change.
+      </p>
+      <h5>Frontend developer h5</h5>
+      <p>
+        We put a monetary on actual life itself. Every 10 years the cells in
+        your body actually completely change. Have you ever tasted Elk heart? so
+        good for you, one of the leanest meats. I've got some i'll give you it
+        on the way out. Look at that ... Yeah, that chimp must be what? 400
+        pounds? Jesus those things will tear you to shreds. Do you know why weed
+        is REALLY illegal?
       </p>
     </div>
   </div>
